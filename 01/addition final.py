ab7c805e8e1179d019e2f5bf0119c9df43f1bafc
dilukshashamal <dilukshashamal2001@gmail.com>
2024-09-26 10:17:18 +0530

    add addition

--- 01/addition final.py
+++ 01/addition final.py
@@ -1,2 +1,11 @@
-print("Hello World")
+import os
+import tensorflow as tf
 
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+X = tf.constant([2.0], name="X")
+Y = tf.constant([4.0], name="Y")
+
+addition = tf.add(X, Y, name="addition")
+
+print(addition.numpy())
