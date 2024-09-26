ac870f61b9ecd48a8b538541ebb40b60ff58b399
dilukshashamal <dilukshashamal2001@gmail.com>
2024-09-26 12:35:54 +0530

    change

--- 01/addition final.py
+++ 01/addition final.py
@@ -3,8 +3,8 @@ import tensorflow as tf
 
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
-X = tf.constant([2.0], name="X")
-Y = tf.constant([4.0], name="Y")
+X = tf.constant([2, 4, 6], name="X")
+Y = tf.constant([4, 1, 3], name="Y")
 
 addition = tf.add(X, Y, name="addition")
 
